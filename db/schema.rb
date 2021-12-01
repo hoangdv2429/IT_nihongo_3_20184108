6593b9135c110c564ec99a530363c72e56e3ae8f
Đỗ Việt Hoàng <doviethoang@Alters-Mac.local>
2021-12-02 02:24:08 +0700

    Add book

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_01_185439) do
+ActiveRecord::Schema.define(version: 2021_12_01_192310) do
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.integer "num_of_pages"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
