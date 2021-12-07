c8f9c693725c377f345dd399eb6228d73368468d
Đỗ Việt Hoàng <doviethoang@Alters-Mac.local>
2021-12-07 11:19:13 +0700

    Add detail book

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_01_202522) do
+ActiveRecord::Schema.define(version: 2021_12_07_041221) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 2021_12_01_202522) do
     t.integer "num_of_pages"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "detail"
   end
 
   create_table "users", force: :cascade do |t|
